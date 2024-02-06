e157a3a6ee4c3a6028c17cb1981c754c0a22a533
pamellam <38889625+pamellam@users.noreply.github.com>
2024-02-06 15:49:36 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const { body } = document;
+
+function changeBackground(number) {
+  // Check if background is already set
+  let prevBackground;
+  if (body.className) {
+    prevBackground = body.className;
+  }
+
+  // Reset CSS class for body
+  body.className = ' ';
+
+  switch (number) {
+    case '1':
+      return prevBackground === 'background-1'
+        ? false
+        : body.classList.add('background-1');
+    case '2':
+      return prevBackground === 'background-2'
+        ? false
+        : body.classList.add('background-2');
+      break;
+    case '3':
+      return prevBackground === 'background-3'
+        ? false
+        : body.classList.add('background-3');
+      break;
+
+    default:
+      break;
+  }
+}
